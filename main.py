ed34818fceec4f558c5383289d83c150de5ee82a
EtamarRothstein <etrothstein@gmail.com>
2026-08-05 15:32:17 +0300

    Restore local versions of main.py and config_parameters.py

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import os
 from pathlib import Path
 import numpy as np
 
+import time
+from config_parameters import *
+
 from statistical_models.tumor_single_gaussian.tumor_single_gaussian_posterior import tumor_single_gaussian_stat_inference
 from statistical_models.healthy_single_gaussian.healthy_single_gaussian_posterior import healthy_single_gaussian_stat_inference
 
-from statistical_models.healthy_single_skew_t.healthy_single_skew_t_posterior import (
-    healthy_single_skew_t_stat_inference,
-)
-
-from statistical_models.tumor_single_skew_t.tumor_single_skew_t_posterior import (
-    tumor_single_skew_t_stat_inference,
-)
+from statistical_models.tumor_single_skew_t.tumor_single_skew_t_posterior import tumor_single_skew_t_stat_inference
+from statistical_models.healthy_single_skew_t.healthy_single_skew_t_posterior import healthy_single_skew_t_stat_inference
 
 from image_processing.visualizations          import visualize_probability,visualize_entropy,visualize_sobel_edges, visualize_contours, visualize_expansion, visualize_segmentation
 from image_processing.compute_entropy         import compute_entropy
@@ -20,7 +18,7 @@ from image_processing.contour_detection       import contour_detection
 from image_processing.contour_classification  import contour_classification
 from image_processing.seed_expansion          import expansion_loop
 
-from evaluation.eval_loop import eval_vol
+from evaluation.eval_loop import eval_vol, eval_dataset
 
 from utilities.utils import load_and_normalize_slice
 
@@ -31,7 +29,7 @@ if __name__ == "__main__":
     # PIPELINE CONFIGURATION & PARAMETERS
     # ==============================================================================
 
-    volume_num = 350
+    volume_num = 302
     slice_num = 90
     target_row = 115
 
@@ -45,18 +43,19 @@ if __name__ == "__main__":
 
 
     # compute full posterior map
-    # GAUSSIAN
-    #healthy_probabilities = healthy_single_gaussian_stat_inference(slice_im, brain_mask)  # Shape: (H, W, K_healthy)
-    #tumor_probabilities   = tumor_single_gaussian_stat_inference  (slice_im, brain_mask)  # Shape: (H, W, K_tumor)
-    #stacked_probabilities = np.dstack([healthy_probabilities, tumor_probabilities])
-    #total_evidence        = np.sum(stacked_probabilities, axis=-1, keepdims=True)
-    #posteriors            = np.divide(stacked_probabilities,total_evidence,out=np.zeros_like(stacked_probabilities),where=total_evidence > 0)
+    if MODEL == "gaussian":
+        healthy_probabilities = healthy_single_gaussian_stat_inference(slice_im, brain_mask)  # Shape: (H, W, K_healthy)
+        tumor_probabilities   = tumor_single_gaussian_stat_inference  (slice_im, brain_mask)  # Shape: (H, W, K_tumor)
+    if MODEL == "skew_t":
+        healthy_probabilities = healthy_single_skew_t_stat_inference(slice_im, brain_mask)  # Shape: (H, W, K_healthy)
+        tumor_probabilities   = tumor_single_skew_t_stat_inference  (slice_im, brain_mask)  # Shape: (H, W, K_tumor)
+    # if MODEL == "gmm":
+    #     healthy_probabilities = #
+    #     tumor_probabilities   = #
+    stacked_probabilities = np.dstack([healthy_probabilities, tumor_probabilities])
+    total_evidence        = np.sum(stacked_probabilities, axis=-1, keepdims=True)
+    posteriors            = np.divide(stacked_probabilities,total_evidence,out=np.zeros_like(stacked_probabilities),where=total_evidence > 0)
 
-    healthy_probabilities = (healthy_single_skew_t_stat_inference(slice_im, brain_mask,))
-    tumor_probabilities = (tumor_single_skew_t_stat_inference(slice_im, brain_mask,))
-    stacked_probabilities = np.dstack([healthy_probabilities,tumor_probabilities,])
-    total_evidence = np.sum(stacked_probabilities, axis=-1, keepdims=True,)
-    posteriors = np.divide(stacked_probabilities, total_evidence, out=np.zeros_like(stacked_probabilities),where=total_evidence > 0,)
 
     entropy_map                       = compute_entropy(posteriors,brain_mask)
     sobel_map                         = sobel_edge_detection(posteriors,brain_mask)
@@ -85,4 +84,8 @@ if __name__ == "__main__":
     fig6_path = os.path.join(figure_output_path, "segmentation_results.png")
     visualize_segmentation(total_segmentation_mask, gt_mask, brain_mask, fig6_path)
 
-    eval_vol(volume_num)
+
+    eval_start = time.perf_counter()
+    eval_dataset(output_directory='evaluation/metrics', model_name='single_gaussian')
+    eval_end = time.perf_counter()
+    print(f'Evaluation Runtime: {(eval_end-eval_start):.3f}')
